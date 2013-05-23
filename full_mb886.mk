ef64d8070fe5252580fe0f0a3b78880c4f3345ca
Peter Carenza <pcarenza@gmail.com>
2013-05-23 15:22:01 -0400

    fixed typo

--- full_mb886.mk
+++ full_mb886.mk
@@ -1,6 +1,5 @@
 $(call inherit-product, device/motorola/mb886/device_mb886.mk)
-PRODUCT_COPY_FILES +=device/motorola/xt925/apns-gsm-conf.xml:system/etc/apns-con
-f.xml
+PRODUCT_COPY_FILES +=device/motorola/xt925/apns-gsm-conf.xml:system/etc/apns-conf.xml
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 ## Device identifier. This must come after all inclusions
