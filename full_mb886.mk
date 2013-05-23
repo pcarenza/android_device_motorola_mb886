49ee5e08632e90d1447750861d39edf0bcf49c6b
Peter Carenza <pcarenza@gmail.com>
2013-05-23 15:18:41 -0400

    reorganized device makefile and added apns

--- full_mb886.mk
+++ full_mb886.mk
@@ -1,6 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 $(call inherit-product, device/motorola/mb886/device_mb886.mk)
+PRODUCT_COPY_FILES +=device/motorola/xt925/apns-gsm-conf.xml:system/etc/apns-con
+f.xml
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mb886
